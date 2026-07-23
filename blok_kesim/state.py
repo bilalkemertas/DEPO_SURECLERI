87a66c9eba4b6d9cccab39990ce370b6b92ee964
bilalkemertas <bilalkemertas@gmail.com>
2026-07-23 16:39:09 +0300

    Refactor BlokKesimState and Messages classes

--- blok_kesim/state.py
+++ blok_kesim/state.py
@@ -1,7 +1,6 @@
 """
 Blok Kesim State Yönetimi
 """
-
 import streamlit as st
 import pandas as pd
 from typing import Dict, List
@@ -15,18 +14,18 @@ def init_blok_kesim_state():
 
 class BlokKesimState:
     """State'i yönet"""
-    
+
     PAGE = 'blok_kesim_page'
     MATCHING_DF = 'eslesme_df'
     WORK_ORDERS = 'operator_work_orders'
     CURRENT_WO_IDX = 'current_work_order_idx'
-    
+
     PAGE_MENU = 'menu'
     PAGE_PLAN = 'plan'
     PAGE_KESIM_MENU = 'kesim_menu'
     PAGE_KESIM = 'kesim'
     PAGE_RAPOR = 'rapor'
-    
+
     @staticmethod
     def init() -> None:
         if BlokKesimState.PAGE not in st.session_state:
@@ -37,41 +36,41 @@ class BlokKesimState:
             st.session_state[BlokKesimState.WORK_ORDERS] = []
         if BlokKesimState.CURRENT_WO_IDX not in st.session_state:
             st.session_state[BlokKesimState.CURRENT_WO_IDX] = None
-    
+
     @staticmethod
     def set_page(page: str) -> None:
         st.session_state[BlokKesimState.PAGE] = page
-    
+
     @staticmethod
     def get_page() -> str:
         return st.session_state.get(BlokKesimState.PAGE, BlokKesimState.PAGE_MENU)
-    
+
     @staticmethod
     def set_matching_df(df: pd.DataFrame) -> None:
         st.session_state[BlokKesimState.MATCHING_DF] = df
-    
+
     @staticmethod
     def get_matching_df() -> pd.DataFrame:
         return st.session_state.get(BlokKesimState.MATCHING_DF, pd.DataFrame())
-    
+
     @staticmethod
     def add_work_order(work_order: Dict) -> None:
         st.session_state[BlokKesimState.WORK_ORDERS].append(work_order)
-    
+
     @staticmethod
     def get_work_orders() -> List[Dict]:
         return st.session_state.get(BlokKesimState.WORK_ORDERS, [])
-    
+
     @staticmethod
     def remove_work_order(idx: int) -> None:
         orders = BlokKesimState.get_work_orders()
         if 0 <= idx < len(orders):
             orders.pop(idx)
-    
+
     @staticmethod
     def clear_work_orders() -> None:
         st.session_state[BlokKesimState.WORK_ORDERS] = []
-    
+
     @staticmethod
     def navigate_to(page: str, clear_work_orders: bool = False) -> None:
         BlokKesimState.set_page(page)
@@ -82,7 +81,7 @@ class BlokKesimState:
 
 class Messages:
     """UI Metinleri"""
-    
+
     HOME_TITLE = "🧱 Blok ve Rulo Sünger Kesim Otomasyonu"
     PLAN_BUTTON = "📋 PLAN & İŞ EMRİ YÜKLE"
     KESIM_BUTTON = "🧱 KESİM OPERASYONU"
